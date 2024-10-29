49a32218dbdae1080a1e122adaa53d24794f06ca
Jackson Severino da Rocha <jackson.severino.rocha@escola.pr.gov.br>
2024-10-28 22:55:33 -0300

    feat: inclide ods and json files

--- scripts/app.py
+++ scripts/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Seleção do tipo de arquivo
 st.title("Escolha o tipo de arquivo para processar")
-file_type = st.selectbox("Selecione o tipo de arquivo:", ["CSV", "XLSX"])
+file_type = st.selectbox("Selecione o tipo de arquivo:", ["CSV", "XLSX", "JSON", "ODS"])
 
 # Execução com base na seleção
 if file_type == "CSV":
@@ -11,3 +11,9 @@ if file_type == "CSV":
 elif file_type == "XLSX":
     import modules.xlsx_process as xlsx_module
     xlsx_module.run()
+elif file_type == "JSON":
+    import modules.json_process as json_module
+    json_module.run()
+elif file_type == "ODS":
+    import modules.ods_process as ods_module
+    ods_module.run()
